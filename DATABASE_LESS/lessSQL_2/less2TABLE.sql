4bd2e3e5d533d762118afa78353fbba60e452481
wevdev <wev@gmail.com>
2019-05-11 21:45:54 +0300

    lodjfdjkf

---
+++ DATABASE_LESS/lessSQL_2/less2TABLE.sql
@@ -0,0 +1,6 @@
+select * from SomeTable where age BETWEEN 18 and 90;
+select * from SomeTable where age > 18 LIMIT 2;
+select * from SomeTable where login LIKE 'q%';
+select * from SomeTable where login LIKE '%e%';
+select * from SomeTable where login LIKE '_e%';
+show tables;
